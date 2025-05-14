b9af7b0162659896c199737a3fc19e87a40af506
Iateam613 <dnosrati@aplsecure.com>
2025-05-14 16:23:36 +0100

    Update settings.py

--- document_reader/settings.py
+++ document_reader/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ['*']
 
 #CORS configuration
 CORS_ALLOWED_ORIGINS = [
-    "document-reader.visa26.com",  # Add your UI's URL here
+    "https://document-reader-one.visa26.com",  # Add your UI's URL here
     "https://app.visa26.com",
 ]
 
